7a5625ea1392f4ddbd1f034cdecb92fec1e37853
justnow <ggjustnow@163.com>
2019-07-16 09:44:44 +0800

    offer_05,用两个栈实现队列。

--- offer_05/src/Test.java
+++ offer_05/src/Test.java
@@ -1,3 +1,8 @@
+/**
+ *用两个栈实现一个队列。队列的声明如下，请实现它的两个函数appendTail和deleteHead，分别完成在队列尾部插入结点和在队列头部删除结点的功能
+ *思路：用两个栈，数据先进入第一个队列然后弹出，将弹出的元素内容按照顺序放入到第二个队列，来模拟队列
+ *
+ */
 public class Test {
     public static void main(String[] args) {
         Solution solution = new Solution();
