f172511e88a2baf2a4a2e2ca45215e9a677e316d
justn0w <ggjustnow@163.com>
2019-10-05 14:44:56 +0800

    offer_09 添加循环的方式

--- offer_09/Readme.md
+++ offer_09/Readme.md
@@ -35,3 +35,34 @@ public class Solution {
     }
 }
 ```
+
+```java
+使用循环：
+
+    /**
+     * 用循环的方式
+     * @param n
+     * @return
+     */
+    public static int totalSolutions2(int n) {
+
+        if (n <= 2) {
+            return n;
+        }
+
+        //初始化两个加数
+        int result=0;
+        int add1 = 1;
+        int add2 = 2;
+
+        //这里i为所对应的台阶数
+        for (int i=3; i <= n; i++){
+            result = add1 + add2;
+            add1 = add2;
+            add2 = result;
+        }
+
+        return result;
+
+    }
+```
--- offer_09/src/com/justnow/offer/Solution.java
+++ offer_09/src/com/justnow/offer/Solution.java
@@ -3,11 +3,17 @@ package com.justnow.offer;
 public class Solution {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            System.out.println(i + "个台阶的方案个数为： " + totalSOlutions(i));
+            System.out.println(i + "个台阶的方案个数为： " + totalSolutions(i));
+            System.out.println(i + "个台阶的方案个数为： " + totalSolutions2(i));
         }
     }
 
-    public static int totalSOlutions(int n){
+    /**
+     * 采用递归的思想
+     * @param n
+     * @return
+     */
+    public static int totalSolutions(int n){
 
         if (n <= 0) {
             return 0;
@@ -16,6 +22,33 @@ public class Solution {
         } else if (n == 2) {
             return 2;
         }
-        return totalSOlutions(n-1) + totalSOlutions(n-2);
+        return totalSolutions(n-1) + totalSolutions(n-2);
+    }
+
+    /**
+     * 用循环的方式
+     * @param n
+     * @return
+     */
+    public static int totalSolutions2(int n) {
+
+        if (n <= 2) {
+            return n;
+        }
+
+        //初始化两个加数
+        int result=0;
+        int add1 = 1;
+        int add2 = 2;
+
+        //这里i为所对应的台阶数
+        for (int i=3; i <= n; i++){
+            result = add1 + add2;
+            add1 = add2;
+            add2 = result;
+        }
+
+        return result;
+
     }
 }
