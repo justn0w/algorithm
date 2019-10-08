2d17b09204c01aad0951919f40913dd6b0bf70e9
justn0w <ggjustnow@163.com>
2019-10-08 11:43:13 +0800

    offer_09_01和offer_10

---
+++ offer_09_01/src/com/justnow/offer/Test.java
@@ -0,0 +1,12 @@
+package com.justnow.offer;
+
+import java.math.BigInteger;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(Integer.toBinaryString(-1));
+        System.out.println(Integer.toBinaryString(-128));
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(BigInteger.valueOf(1));
+    }
+}
