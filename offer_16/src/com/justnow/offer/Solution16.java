a99c18c0a08fde07afe67baab1406b240d95742f
justn0w <ggjustnow@163.com>
2019-10-21 10:41:19 +0800

    用贪婪算法动态规划的问题

--- offer_16/src/com/justnow/offer/Solution16.java
+++ offer_16/src/com/justnow/offer/Solution16.java
@@ -1,21 +1,16 @@
 package com.justnow.offer;
 
+import static java.lang.StrictMath.pow;
+
 public class Solution16 {
     public static void main(String[] args) {
         int n = 8;
         System.out.println(getMaxResult(8));
         //边界测试
-        for (int i=0; i < 5; i++) {
+        for (int i=0; i < 10; i++) {
             System.out.println(getMaxResult(i));
+            System.out.println(getMaxResult2(i));
         }
-
-        /**
-         * 对应的测试结果
-         * 1
-         * 1
-         * 2
-         * 4
-         */
     }
 
     public static int getMaxResult(int length) {
@@ -48,5 +43,31 @@ public class Solution16 {
         }
         max = results[length];
         return max;
+    }
+
+    /**
+     * 使用贪婪算法
+     * @param length
+     * @return
+     */
+    public static int getMaxResult2(int length) {
+        // 如果length小于等于3，则直接输出结果
+        if (length < 2)
+            return 1;
+        if (length ==2)
+            return 1;
+        if (length == 3)
+            return 2;
+        //求出可以切割成3的个数
+        int timesof3 = length / 3;
+        //当以3为单位来切割时，如果最后一段的长度为4，需求将最后一段按照2+2来切割
+        if (length - timesof3 * 3 == 1) {
+            timesof3--;
+        }
+
+        //求出可以剩余可以切割为2的个数
+        int timesof2 = (length - timesof3 * 3) / 2;
+
+        return (int)(pow(3, timesof3)) * (int)(pow(2, timesof2));
     }
 }
